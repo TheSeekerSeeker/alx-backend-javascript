830c149c5838bd856bc1d65f98e39ee44850c4bd
ezra odondi <odondiezra5@gmail.com>
2023-01-05 00:04:57 +0300

    0x04. Typescript

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,61 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [x: string]: any;
+}
+
+// solution for task 2
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// solution for task 3
+interface IStudentClassProps {
+  firstName: string;
+  lastName: string;
+}
+
+interface IStudent {
+  displayName: string;
+  workOnHomework: () => string;
+}
+
+class StudentClass implements IStudent {
+  displayName: string;
+
+  constructor(props: IStudentClassProps) {
+    this.displayName = props.firstName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+}
+
+const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+) => `${firstName.substring(0, 1)}. ${lastName}`;
+
+// Examples:
+const B = document.querySelector('body');
+// task 1:
+const SECTION_T1 = document.createElement('SECTION');
+const H2_T1 = document.createElement('H2');
+H2_T1.innerText = 'Task 1';
+SECTION_T1.appendChild(H2_T1);
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
